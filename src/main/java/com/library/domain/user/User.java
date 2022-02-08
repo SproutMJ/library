1b7e917fe233488553468c5b015ca111bd1cf810
minjun <mjk1234progma@gmail.com>
2022-02-08 22:16:11 +0900

    중간커밋

---
+++ src/main/java/com/library/domain/user/User.java
@@ -0,0 +1,12 @@
+package com.library.domain.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class User {
+}
